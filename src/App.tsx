dd3bcd52c836ae1e3d1076273cecdb6b3adb25b0
Brandon Gomez <bgomez9212@gmail.com>
2024-07-14 12:41:09 -0700

    tanstack query set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+
 export default function App() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["articles"],
+    queryFn: () =>
+      fetch("http://127.0.0.1:8000/demo/articles/").then((res) => res.json()),
+  });
+  console.log(data);
   return (
     <div className="px-20 py-10">
       <h1>Hello world</h1>
