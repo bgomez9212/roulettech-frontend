a830a23501d162c7098e790b4b8a9027d2d10d39
Brandon Gomez <bgomez9212@gmail.com>
2024-07-18 13:16:20 -0700

    additional formatting details

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -6,10 +6,10 @@ export default function NavigationBar({ toggleModal }: NavigationBarProps) {
   return (
     <div>
       <div className="h-20 border flex flex-row justify-between px-5 md:px-20 items-center fixed bg-white w-[100vw]">
-        <h1>Absolute Albums</h1>
+        <h1 className="text-2xl">Absolute Albums</h1>
         <button
           onClick={toggleModal}
-          className="h-1/2 px-2 rounded-xl items-center justify-center flex border"
+          className="h-1/2 px-2 rounded-xl items-center justify-center flex border bg-black text-white hover:opacity-50"
         >
           + Add an album
         </button>
